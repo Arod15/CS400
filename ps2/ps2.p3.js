dcb8c52594971e99f0c4a901fb5f725c84c3ea51
Arod15 <Arod1198@gmail.com>
2020-02-11 11:46:41 -0500

    changed code from expression into executing 1 line function call

--- ps2/ps2.p3.js
+++ ps2/ps2.p3.js
@@ -1,1 +1,3 @@
-[1,2,3,4,5,6,7].map(x => console.log(Math.pow(x, 3)));
+// [1,2,3,4,5,6,7].map(x => console.log(Math.pow(x, 3)));
+// the original code is above. please don't hurt me if the ugliness outweighs the 'conciseness' :)
+(function() { [1,2,3,4,5,6,7].map(x => console.log(Math.pow(x, 3))) })();
