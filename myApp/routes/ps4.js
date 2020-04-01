63e32aeea5a5be91ad7e186db3bcd4dfd5c34838
Arod15 <Arod1198@gmail.com>
2020-04-01 14:03:50 -0400

    finised assignment

---
+++ myApp/routes/ps4.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+const https = require('https')
+const options = {
+  hostname: 'www.boredapi.com',
+  path: '/api/activity',
+  method: 'GET'
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    // Following external GET request code taken from https://nodejs.dev/making-http-requests-with-nodejs
+    const req1 = https.request(options, resp => {
+        console.log(`statusCode: ${resp.statusCode}`)
+        
+        resp.on('data', d => {
+            res.render('ps4Template', {title: 'Express', activity: JSON.parse(d.toString()).activity, results: d.toString()});
+        })
+    })
+    
+    req1.on('error', error => {
+        console.error(error)
+    })
+    
+    req1.end()
+});
+
+module.exports = router;
